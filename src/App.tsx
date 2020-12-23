491f2b32ba0f7287231196cb86c3d662db4a5e57
tokuyama shouta <toku1031ts@gmail.com>
2020-12-23 19:27:04 +0900

    useEffext cleanup関数(TypeScript)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React,{ useState, useEffect } from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import CleanUp from './CleanUp';
 
 
 const App:React.FC = () => {
   const [status, setStatus ] = useState<string | number>("text");
   const [input, setInput] = useState("");
   const [counter, setCounter] = useState(0);
+  const [display, setDisplay] = useState(true);
 
   const onChangeHandler = (e:React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value)
@@ -26,6 +28,8 @@ const App:React.FC = () => {
         <input type="text" value={input} onChange={onChangeHandler}/>
         <h4>{counter}</h4>
         <button onClick={() => setCounter((preCounter) => preCounter + 1)}>Increment</button>
+        {display && <CleanUp />}
+        <button onClick={() => setDisplay(!display)}>Toggle display</button>
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
